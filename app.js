9fa2153c4b326bcde20c3d775e49757ab3798804
abdumajidov23 <alijonabdumajidov09@gmail.com>
2024-07-11 07:03:35 +0500

    uy_ishi 16

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("Assalomu alaykum ustoz. ustoz ulgurmadim kechgacha vaqt bering oxiriga yetkazib qoyaman")
+
+
+// const fetch = require('node-fetch');
+// async function getUserDetails(userId) {
+//   try {
+//     const userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+//     const userData = await userResponse.json();
+
+//     const postsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+//     const postsData = await postsResponse.json();
+
+//     console.log(`Avtor: ${userData.name}`);
+//     console.log(`Shahar: ${userData.address.city}`);
+//     console.log(`Kompaniya: ${userData.company.name}`);
+
+//     console.log(`Maqolalar:`);
+//     postsData.forEach(post => {
+//       console.log(`- ${post.title}`);
+//     });
+//   } catch (error) {
+//     console.error(`Xatolik: ${error.message}`);
+//   }
+// }
+
+// getUserDetails(1);
