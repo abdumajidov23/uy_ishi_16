1e00bb41a451dc712833238d4b27fae6a826b5a3
abdumajidov23 <alijonabdumajidov09@gmail.com>
2024-07-11 09:11:05 +0500

    uy_ishi 16_1

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-console.log("Assalomu alaykum ustoz. ustoz ulgurmadim kechgacha vaqt bering oxiriga yetkazib qoyaman")
-
-
-// const fetch = require('node-fetch');
-// async function getUserDetails(userId) {
-//   try {
-//     const userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
-//     const userData = await userResponse.json();
-
-//     const postsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-//     const postsData = await postsResponse.json();
-
-//     console.log(`Avtor: ${userData.name}`);
-//     console.log(`Shahar: ${userData.address.city}`);
-//     console.log(`Kompaniya: ${userData.company.name}`);
-
-//     console.log(`Maqolalar:`);
-//     postsData.forEach(post => {
-//       console.log(`- ${post.title}`);
-//     });
-//   } catch (error) {
-//     console.error(`Xatolik: ${error.message}`);
-//   }
-// }
-
-// getUserDetails(1);
+async function getUserAndPosts(userId) {
+      try {
+            const user_response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+            const user_data = await user_response.json()
+            console.log(
+`name: ${user_data.name},
+city: ${user_data.address.city},
+companyname: ${user_data.company.name}
+          `);
+            
+                const post_response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+                const post_data = await post_response.json()
+            
+                console.log('Titles of all posts:');
+                post_data.forEach(post => {
+                      console.log(post.id,post.title);
+                    });
+                
+                  } catch (error) {
+                        console.log(error);
+                      }
+                    }
+                    getUserAndPosts(1);
